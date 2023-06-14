e687f351735806782986521b7f53e3db5032e26b
Farman Tasleem <iamfarmanmalik@gmail.com>
2023-06-14 18:40:01 +0530

    github user find built

--- githubuser/src/App.js
+++ githubuser/src/App.js
@@ -3,18 +3,25 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const[userName,setuserName]=useState("")
 
-  const[userData,setuserData]=useState({name:"",bio:"",img:"",followers:0,pb:0,portfolio:0,location:""})
+  async function getData(){    
+    const resp=await fetch(`https://api.github.com/users/${userName}`);
+    const {avatar_url,bio,followers,location,name,blog,public_repos}=await resp.json()
+    setuserData({name,bio,img:avatar_url,followers,location,portfolio:blog,pr:public_repos})
+  }
+
+  const[userData,setuserData]=useState({name:"",bio:"",img:"",followers:0,pr:0,portfolio:0,location:""})
   return (
     <>
     <div id="searchbar">
-    <input id="userName" type="text" placeholder="Enter User Name: Eg. farmantasleem"/>
-    <button onclick="getData()">Get Data</button>
+    <input id="userName" value={userName} onChange={(e)=>{setuserName(e.target.value)}} type="text" placeholder="Enter User Name: Eg. farmantasleem"/>
+    <button onClick={getData}>Get Data</button>
 
 
 </div>
-<div id="content">
-    <img id="image" src="https://th.bing.com/th/id/OIP.Kc2eyEH1-Xc6Cl3ltZCFNAAAAA?pid=ImgDet&w=360&h=360&rs=1" alt=""/>
+<div id="content" style={{display:userData.name?"flex":"none"}}>
+    <img id="image" src={userData.img} alt=""/>
     <div>
         <div>
             <h1>Name : <span id="name">{userData.name||"Name"}</span></h1>
