46cd29b9e89c7a8196640c73c63bf3a76d967b99
Farman Tasleem <iamfarmanmalik@gmail.com>
2023-06-14 18:18:34 +0530

    header built

---
+++ ecommercewebapp/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export const Home=()=>{
+    return(
+        <div id="home">
+            <header>
+                    <a href="http://">Home</a>
+                    <a href="http://">Product</a>
+                    <a href="http://">About</a>
+            </header>                
+        </div>
+    )
+}
