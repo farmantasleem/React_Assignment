78588403c238a98de74593371cfe09e2913ae690
Farman Tasleem <iamfarmanmalik@gmail.com>
2023-06-15 11:14:49 +0530

    show product init

---
+++ showproduct/src/App.jsx
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id='showproduct'>
+        <div className='productCard'>
+              <img src='https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'/>
+              <div className='content'>
+                  <h1>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h1>
+                  <p>Price: 120 inr</p>
+                  <button>Add to Cart</button>
+                  <button>Buy Now</button>
+              </div>
+              
+
+        </div>
+
+        <div className='productCard'>
+              <img src='https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'/>
+              <div className='content'>
+                  <h1>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h1>
+                  <p>Price: 120 inr</p>
+                  <button>Add to Cart</button>
+                  <button>Buy Now</button>
+              </div>
+              
+
+        </div>
+        <div className='productCard'>
+              <img src='https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'/>
+              <div className='content'>
+                  <h1>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h1>
+                  <p>Price: 120 inr</p>
+                  <button>Add to Cart</button>
+                  <button>Buy Now</button>
+              </div>
+              
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
